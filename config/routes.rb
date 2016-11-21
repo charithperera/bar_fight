9de3f59e2e61d8fd14a8398ee8912b31e113fd89
Charith Perera <charith.p@gmail.com>
2016-11-22 09:15:51 +1100

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#index"
   get "/signup", to: "session#signup"
+  get "/login", to: "session#login"
   get "/collection", to: "game#collection"
   get "/opponents", to: "game#opponents"
   get "/requestbattle", to: "game#requestbattle"
   get "/beginbattle", to: "game#beginbattle"
+
 
 
   post "/login", to: "session#login"
