97ca99d391ed4bfd2bb3a1880dc9b0863e5a481e
CKS <ckseng86@gmail.com>
2016-11-22 12:26:58 +1100

    temporary changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#index"
+  get "/main", to: "pages#main"
   get "/signup", to: "session#signup"
   get "/login", to: "session#index"
   get "/collection", to: "game#collection"
