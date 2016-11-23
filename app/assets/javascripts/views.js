8a7fec4f456ebf2dda859da55b1581c51df6cbaa
Kerrie Batrouney <batrouney@gmail.com>
2016-11-24 09:24:20 +1100

    spinner added and dashboard half styled

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -10,7 +10,8 @@ var $exitGameBtn = $('#exitgame');
 var $menu = $('.menu');
 var $dashBoardScreen = $(".dashboard");
 var $newGameScreen = $(".newgame");
-
+var $returnBtn1 = $("#returnFromDashboard");
+var $returnBtn2 = $("#returnFromGame");
 
 $newGameBtn.on('click', function(e){
   $menu.toggle();
@@ -26,7 +27,15 @@ $exitGameBtn.on('click', function(e){
   console.log(e.target);
   // logout and return to home
 
-
 })
 
+$returnBtn1.on('click', function(e){
+  $menu.toggle();
+  $dashBoardScreen.toggle();
+})
+
+$returnBtn2.on('click', function(e){
+  $menu.toggle();
+  $newGameScreen.toggle();
+})
 })
