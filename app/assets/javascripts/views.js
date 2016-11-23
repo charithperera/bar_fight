90f9970aac57f4438cbb6ffc8e8eca12d78b2197
CKS <ckseng86@gmail.com>
2016-11-23 22:02:46 +1100

    add exit game, set background image attachment to fixed, fix indentations

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -1,32 +1,20 @@
 
-
-
-
 $(document).ready(function(){
 
-var $newGameBtn = $('#newgame');
-var $dashBoardBtn = $('#dashboard');
-var $exitGameBtn = $('#exitgame');
-var $menu = $('.menu');
-var $dashBoardScreen = $(".dashboard");
-var $newGameScreen = $(".newgame");
+  var $newGameBtn = $('#newgame');
+  var $dashBoardBtn = $('#dashboard');
+  var $menu = $('.menu');
+  var $dashBoardScreen = $(".dashboard");
+  var $newGameScreen = $(".newgame");
 
+  $newGameBtn.on('click', function(e){
+    $menu.toggle();
+    $newGameScreen.toggle();
+  })
 
-$newGameBtn.on('click', function(e){
-  $menu.toggle();
-  $newGameScreen.toggle();
-})
-
-$dashBoardBtn.on('click', function(e){
-  $menu.toggle();
-  $dashBoardScreen.toggle();
-})
-
-$exitGameBtn.on('click', function(e){
-  console.log(e.target);
-  // logout and return to home
-
-
-})
+  $dashBoardBtn.on('click', function(e){
+    $menu.toggle();
+    $dashBoardScreen.toggle();
+  })
 
 })
