2432044a71b10fb4d09249718d5e0c15a20de04d
Charith Perera <charith.p@gmail.com>
2016-11-24 14:18:33 +1100

    FIXED SO MANY ISSUES

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,14 +6,6 @@ class GameController < ApplicationController
     new_game.opponent_id = params["opponentId"]
     new_game.challenger_id = current_user.id
     new_game.save
-<<<<<<< HEAD
-=======
-
-  end
-
-  def acceptbattle
-    # update user to say in game
->>>>>>> e2d344cad087653762becb2e2d7e25303c544dba
   end
 
   def beginbattle
